18c1fea17858a5e5989fe1a4d8380a9b4d4c98c7
Michael COULLERET <michael@coulleret.pro>
2018-01-05 17:01:41 +0100

    [Issue #7] run image with a non-root user

--- build.sh
+++ build.sh
@@ -1,21 +1,10 @@
 #!/bin/bash
 
-# for returning later to the main directory
-ROOT_DIRECTORY=./
-
-# read repository configuration
+ROOT_DIRECTORY=`pwd`
 source $ROOT_DIRECTORY/vars
 
-# for each enabled repository
 for REPOSITORY in $REPOSITORIES; do
-    # read repository configuration
-    source $ROOT_DIRECTORY/vars
-
-    # build all enabled versions
     for TAG in $TAGS; do
-        # some verbose
-        echo $'\n\n'"--> Building $NAMESPACE:$TAG"$'\n'
-
         for VARI in $VARIANT; do
             if [ -d "$ROOT_DIRECTORY/$REPOSITORY/$TAG/$VARI" ]; then
                 echo $"---> Building variant $NAMESPACE:$TAG-$VARI"$'\n'
